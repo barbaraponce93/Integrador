3aa6936641a7b6e93b7b87569ed6776ff50a5a1c
barbaraponce93 <141653104+barbaraponce93@users.noreply.github.com>
2024-03-03 19:07:29 -0300

    header y footer dinamico

--- js/cabecera.js
+++ js/cabecera.js
@@ -1,15 +1,114 @@
+
+////////////////////////////////////////////////////////////////////////
+// Función para cargar el encabezado y el pie de página dinámicamente
+document.addEventListener('DOMContentLoaded', function () {//el evento DOMContentLoaded se asegura  de que el DOM esté completamente cargado antes de ejecutar otra funcion. 
+
+    cargarHeaderFooter();
+});
+
+function cargarHeaderFooter() {
+    
+    const headerDinamico = document.getElementById('header-dinamico');
+    //le pasamos el contenido HTML del encabezado 
+    const contenidoHeader = `          
+    <div class="header-contenedor">
+    <div class="header-atencionPublico">
+        <p class="parrafo">Hablanos de Lunes a viernes de 09:00 a 18:00hs</p>
+    </div>
+
+    <div class="header-logo">
+        <img src="../img/logo-transparente.png" alt="" id="logo-img">
+    </div>
+
+    <div class="header-buscador">
+        <input type="text" id="lupa">
+        <span><img src="../img/lupa.png" alt="" class="lupa-icono"></span>
+    </div>
+
+    <div class="header-carrito">
+        <img src="../img/carrito.png" alt="">
+        <p>Mi carrito</p>
+    </div>
+
+</div>
+
+<nav class="header-menubar">
+    <!-- <input type="text">-->
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="productos.html">Productos</a></li>
+        <li><a href="contactos.html">Contactos</a></li>
+    </ul>
+</nav>
+    `;
+    headerDinamico.innerHTML = contenidoHeader; //insertamos dentro de nuestro elemento dom ('header-dinamico') la cadena de texto que contiene el html
+
+    // Cargar el pie de página
+    const footerDinamico = document.getElementById('footer-dinamico');
+    const contenidofooter = `
+    <div class="contenedor-footer">
+
+    <div class="navegacion">
+        <span>Navegacion</span>
+        <ul class="nav-footer">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="productos.html">Productos</a></li>
+            <li><a href="contactos.html">Contactos</a></li>
+
+        </ul>
+
+    </div>
+
+    <div class="contacto">
+        <span>Contáctanos</span>
+        <ul>
+            <li><img src="../img/whatsapp.png" alt="" class="contacto-img"> &nbsp; 351123456</li>
+            <li><img src="../img/mail.png" alt="" class="contacto-img">&nbsp; blancosdachary@gmail.com</li>
+            <li><img src="../img/ubicacion.png" alt="" class="contacto-img"> &nbsp;La paz 175</li>
+        </ul>
+    </div>
+
+    <div class="tarjetas">
+        <span>Medios de pago</span>
+        <ul>
+            <li><img src="../img/americanexpress.png" alt="" class="img-tarjetas"></li>
+            <li><img src="../img/cabal.png" alt="" class="img-tarjetas"></li>
+            <li><img src="../img/cencosud.png" alt="" class="img-tarjetas"></li>
+            <li><img src="../img/dinersclubtarjeta.png" alt="" class="img-tarjetas"></li>
+            <br>
+            <li><img src="../img/mastercard.png" alt="" class="img-tarjetas"></li>
+            
+            <li><img src="../img/pagofacil.png" alt="" class="img-tarjetas"></li>
+            <li><img src="../img/rapipago.png" alt="" class="img-tarjetas"></li>
+            <li><img src="../img/tarjeta-naranja.png" alt="" class="img-tarjetas"></li>
+            <br>
+            <li><img src="../img/visa.png" alt="" class="img-tarjetas"></li>
+            <li><img src="../img/americanexpress.png" alt="" class="img-tarjetas"> </li>
+        </ul>
+    </div>
+
+</div>
+<div class="copy-footer">
+    <p> &copy Copyright Grupo 4 - 2024</p>
+</div>
+    `;
+    footerDinamico.innerHTML = contenidofooter;
+}
+
+///////////////////////////////////////////////
 const parrafo = document.querySelector('.parrafo');
 
 function cambiarTexto(nuevoTexto) {
-  parrafo.textContent = nuevoTexto;//cambiamos el contenido parrafo
+    const parrafo = document.querySelector('.parrafo');
+    if (parrafo) {
+        parrafo.textContent = nuevoTexto;
+    }
 }
 
-setInterval(() => {//esta funcion va a ejecutar a cambiar texto y le dice que lo cambie cada 4s en este caso
-  cambiarTexto('Whatsapp 351123456');
-}, 4000); // 4s
+setInterval(() => {
+    cambiarTexto('Whatsapp 351123456');
+}, 4000);
 
 setInterval(() => {
-  cambiarTexto('Hablanos de Lunes a viernes de 09:00 a 18:00hs');
-}, 6000); // 6s
-
-////////////////////////////////////////////////////////////////////////
+    cambiarTexto('Hablanos de Lunes a viernes de 09:00 a 18:00hs');
+}, 6000);
